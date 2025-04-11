f955160716155fbab546b47028ca72ce60be9dcd
Swetha Radhakrishnan <187436640+Swetha-Krish@users.noreply.github.com>
2025-04-10 23:46:30 -0500

    Add 217_Contains_Duplicate py solution

---
+++ Hashmaps_and_Sets/217_Contains_Duplicate/contains_duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(nums) != len(set(nums)):
+            return True
+        else: 
+            return False
+
+#Time complexity: O(n)
+#Space complexity: O(n)
